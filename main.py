a4c1503b5b602f95726df61617a0e9de14a9ca2f
Nathan lee <nathan.l.lee123@gmail.com>
2019-02-20 06:11:01 +0000

    Got closer to solving streaming issue

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tweepy
 from tweepy import OAuthHandler
 import datetime
-
+import time
 auth = OAuthHandler('CXMg9Pt0OHWeTIPpD46NCqNaK', 'nT0K3Z9oNQjX7Zu64WBiFCjCPs9vTSDPwy8PSBUOOLqzRKTA9r')
 auth.set_access_token('807388883944017920-7WYXJ0RRp6sqzdeSNktJ3OLmSK8AnLc', 'rqupZ2ScfrIYJ5SlJuRxOC8IIvHVx6bdqGeuCnlXJ4S10')
 
@@ -9,6 +9,7 @@ keyworddict = {
 'happy' : ['happiest', 'so happy', 'excited', 'joyful', 'smile'],
 'sad' : ['i\'m so sad', 'i\'m heartbroken', 'i am depressed', 'i am hurting']
 }
+happy = ['happiest', 'so happy', 'excited', 'joyful', 'smile']
 
 
 class Streamer(tweepy.StreamListener):
@@ -16,12 +17,16 @@ class Streamer(tweepy.StreamListener):
     def on_status(self, status):
         data = []
         counter = 0
-        print(status.text)
+        try:
+            print(status.text)
+        except:
+            time.sleep(.5)
         data.append(status.text)
         counter += 1
-
+        return len(data)
     def on_error(self, status_code):
         print (status_code)
+        print('errrorror')
 
 def collectdata(keywords, counter):
     streamer = Streamer()
@@ -32,3 +37,7 @@ def collectdata(keywords, counter):
 
         stream.filter(track=keywordset)
         datacollected.append({keywordset.keys() : counter})
+
+streamer = Streamer()
+stream = tweepy.Stream(auth = auth, listener=Streamer())
+stream.filter(track=happy)
